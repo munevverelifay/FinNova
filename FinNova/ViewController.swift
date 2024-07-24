c3c87eff8bac7f98defb841f7e0929eab5eda512
Elif Ay <munevverelifay@gmail.com>
2024-07-24 20:18:08 +0300

    Initial Commit

---
+++ FinNova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinNova
+//
+//  Created by Münevver Elif Ay on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
